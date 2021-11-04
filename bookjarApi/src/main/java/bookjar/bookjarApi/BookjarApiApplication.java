44845f85f9f5b9aa1a5b39ecfc87537887748df5
Ana Paula Tartari <anatartari412@gmail.com>
2021-11-04 18:42:20 -0300

    Inital project

---
+++ bookjarApi/src/main/java/bookjar/bookjarApi/BookjarApiApplication.java
@@ -0,0 +1,13 @@
+package bookjar.bookjarApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookjarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookjarApiApplication.class, args);
+	}
+
+}
