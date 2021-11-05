ef361b76eca57586651ef5ca7f9bbc1c5e4658a8
Ana Paula Tartari <anatartari412@gmail.com>
2021-11-04 22:47:51 -0300

    Tables

---
+++ bookjarApi/src/main/java/bookjar/bookjarApi/Models/Book.java
@@ -0,0 +1,206 @@
+package bookjar.bookjarApi.Models;
+
+import java.sql.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="Books")
+public class Book {
+    
+    public Book(){}
+
+    public Book(int id, String title, String author, String edition, int rating, int status, Date readAt,
+            String comment, String color, bookjar.bookjarApi.Models.User user) {
+        Id = id;
+        Title = title;
+        Author = author;
+        Edition = edition;
+        Rating = rating;
+        Status = status;
+        ReadAt = readAt;
+        Comment = comment;
+        Color = color;
+        User = user;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int Id;
+
+    @Column(length = 150)
+    private String Title;
+
+    @Column(length = 80)
+    private String Author;
+
+    private String Edition;
+
+    private int Rating;
+
+    private int Status;
+
+    private Date ReadAt;
+
+    @Column(length = 300)
+    private String Comment;
+
+    private String Color;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id", referencedColumnName = "bookId")
+    @JsonIgnoreProperties("booksList")
+    private User User;
+
+    /**
+     * @return int return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return String return the Title
+     */
+    public String getTitle() {
+        return Title;
+    }
+
+    /**
+     * @param Title the Title to set
+     */
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    /**
+     * @return String return the Author
+     */
+    public String getAuthor() {
+        return Author;
+    }
+
+    /**
+     * @param Author the Author to set
+     */
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+
+    /**
+     * @return String return the Edition
+     */
+    public String getEdition() {
+        return Edition;
+    }
+
+    /**
+     * @param Edition the Edition to set
+     */
+    public void setEdition(String Edition) {
+        this.Edition = Edition;
+    }
+
+    /**
+     * @return int return the Rating
+     */
+    public int getRating() {
+        return Rating;
+    }
+
+    /**
+     * @param Rating the Rating to set
+     */
+    public void setRating(int Rating) {
+        this.Rating = Rating;
+    }
+
+    /**
+     * @return int return the Status
+     */
+    public int getStatus() {
+        return Status;
+    }
+
+    /**
+     * @param Status the Status to set
+     */
+    public void setStatus(int Status) {
+        this.Status = Status;
+    }
+
+    /**
+     * @return Date return the ReadAt
+     */
+    public Date getReadAt() {
+        return ReadAt;
+    }
+
+    /**
+     * @param ReadAt the ReadAt to set
+     */
+    public void setReadAt(Date ReadAt) {
+        this.ReadAt = ReadAt;
+    }
+
+    /**
+     * @return String return the Comment
+     */
+    public String getComment() {
+        return Comment;
+    }
+
+    /**
+     * @param Comment the Comment to set
+     */
+    public void setComment(String Comment) {
+        this.Comment = Comment;
+    }
+
+    /**
+     * @return String return the Color
+     */
+    public String getColor() {
+        return Color;
+    }
+
+    /**
+     * @param Color the Color to set
+     */
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    /**
+     * @return User return the User
+     */
+    public User getUser() {
+        return User;
+    }
+
+    /**
+     * @param User the User to set
+     */
+    public void setUser(User User) {
+        this.User = User;
+    }
+
+}
---
+++ bookjarApi/src/main/java/bookjar/bookjarApi/Models/User.java
@@ -0,0 +1,211 @@
+package bookjar.bookjarApi.Models;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name="Users")
+public class User {
+
+    public User() {
+    }
+
+    public User(int id, String email, String fullName, String password, Date birthday, String instagram, String twitter,
+            String tiktok, String description, ArrayList<Book> booksList) {
+        Id = id;
+        Email = email;
+        FullName = fullName;
+        Password = password;
+        Birthday = birthday;
+        Instagram = instagram;
+        Twitter = twitter;
+        Tiktok = tiktok;
+        Description = description;
+        this.booksList = booksList;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int Id;    
+
+    @Column(nullable = false, unique = true)
+    private String Email;
+    
+    @Column(nullable = false)
+    private String FullName;
+
+    @Column(nullable = false)
+    private String Password;
+
+    private Date Birthday;
+
+    private String Instagram;
+
+    private String Twitter;
+
+    private String Tiktok;
+
+    @Column(length = 300)
+    private String Description;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "book", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private ArrayList<Book> booksList;
+
+
+    /**
+     * @return int return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return String return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    /**
+     * @return String return the Password
+     */
+    public String getPassword() {
+        return Password;
+    }
+
+    /**
+     * @param Password the Password to set
+     */
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+
+    /**
+     * @return String return the FullName
+     */
+    public String getFullName() {
+        return FullName;
+    }
+
+    /**
+     * @param FullName the FullName to set
+     */
+    public void setFullName(String FullName) {
+        this.FullName = FullName;
+    }
+
+    /**
+     * @return Date return the Birthday
+     */
+    public Date getBirthday() {
+        return Birthday;
+    }
+
+    /**
+     * @param Birthday the Birthday to set
+     */
+    public void setBirthday(Date Birthday) {
+        this.Birthday = Birthday;
+    }
+
+    /**
+     * @return String return the Instagram
+     */
+    public String getInstagram() {
+        return Instagram;
+    }
+
+    /**
+     * @param Instagram the Instagram to set
+     */
+    public void setInstagram(String Instagram) {
+        this.Instagram = Instagram;
+    }
+
+    /**
+     * @return String return the Twitter
+     */
+    public String getTwitter() {
+        return Twitter;
+    }
+
+    /**
+     * @param Twitter the Twitter to set
+     */
+    public void setTwitter(String Twitter) {
+        this.Twitter = Twitter;
+    }
+
+    /**
+     * @return String return the Tiktok
+     */
+    public String getTiktok() {
+        return Tiktok;
+    }
+
+    /**
+     * @param Tiktok the Tiktok to set
+     */
+    public void setTiktok(String Tiktok) {
+        this.Tiktok = Tiktok;
+    }
+
+    /**
+     * @return String return the Description
+     */
+    public String getDescription() {
+        return Description;
+    }
+
+    /**
+     * @param Description the Description to set
+     */
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+
+    /**
+     * @return ArrayList<Book> return the booksList
+     */
+    public ArrayList<Book> getBooksList() {
+        return booksList;
+    }
+
+    /**
+     * @param booksList the booksList to set
+     */
+    public void setBooksList(ArrayList<Book> booksList) {
+        this.booksList = booksList;
+    }
+
+}
