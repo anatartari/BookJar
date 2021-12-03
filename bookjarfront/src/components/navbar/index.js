abdd80831653368c3b3cab76b27550e59ecdb40c
Beatriz Calazans <beatriz.calazans@hotmail.com>
2021-12-03 11:41:03 -0300

    logout

--- bookjarfront/src/components/navbar/index.js
+++ bookjarfront/src/components/navbar/index.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import React from "react";
 import "./navbar.css";
-import { FaBars } from "react-icons/fa";
 import {
   Nav,
   NavbarContainer,
@@ -18,6 +17,7 @@ import Search from "../../assets/icons/search.svg";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles({
   menuItem: {
@@ -27,6 +27,7 @@ const useStyles = makeStyles({
 });
 
 const NavBar = ({ toggle }) => {
+  let navigate = useNavigate();
   const classes = useStyles();
   const userId = localStorage.getItem("@bookjar/userId");
 
@@ -36,7 +37,14 @@ const NavBar = ({ toggle }) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
+    console.log(userId);
     setAnchorEl(null);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("@bookjar/userId");
+    localStorage.removeItem("@bookjar/userEmail");
+    navigate("/");
   };
 
   return (
@@ -51,41 +59,51 @@ const NavBar = ({ toggle }) => {
                 <img className="nav-icon__search" src={Search} />
               </NavLinks>
             </NavItem>
-            <NavItem>
-              <button className="menu-btn" onClick={handleClick}>
-                <img className="nav-icon__book-close" src={BookClose} />
-              </button>
 
-              <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                }}
-              >
-                <Link className="link" to={`/${userId}`}>
-                  <MenuItem className={classes.menuItem} onClick={handleClose}>
-                    Perfil
-                  </MenuItem>
-                </Link>
-                <Link className="link" to="/edit-user">
-                  <MenuItem className={classes.menuItem} onClick={handleClose}>
-                    Editar perfil
-                  </MenuItem>
-                </Link>
-                <Link className="link" to="/register-book">
-                  <MenuItem className={classes.menuItem}>
-                    Adicionar livro
-                  </MenuItem>
-                </Link>
+            {userId === null ? (
+              <></>
+            ) : (
+              <NavItem>
+                <button className="menu-btn" onClick={handleClick}>
+                  <img className="nav-icon__book-close" src={BookClose} />
+                </button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <Link className="link" to={`/profile/${userId}`}>
+                    <MenuItem
+                      className={classes.menuItem}
+                      onClick={handleClose}
+                    >
+                      Perfil
+                    </MenuItem>
+                  </Link>
+                  <Link className="link" to="/edit-user">
+                    <MenuItem
+                      className={classes.menuItem}
+                      onClick={handleClose}
+                    >
+                      Editar perfil
+                    </MenuItem>
+                  </Link>
+                  <Link className="link" to="/register-book">
+                    <MenuItem className={classes.menuItem}>
+                      Adicionar livro
+                    </MenuItem>
+                  </Link>
 
-                <MenuItem className={classes.menuItem} onClick={handleClose}>
-                  Sair dessa conta
-                </MenuItem>
-              </Menu>
-            </NavItem>
+                  <MenuItem className={classes.menuItem} onClick={logout}>
+                    Sair dessa conta
+                  </MenuItem>
+                </Menu>
+              </NavItem>
+            )}
           </NavMenu>
         </NavbarContainer>
       </Nav>
